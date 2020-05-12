3054c98cdfddd7b255618f07cb2f9bc79af197e2
sunhui44311 <1874314758@qq.com>
2020-05-12 14:15:14 +0800

    updata

---
+++ pages/user/info/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <view id="info">
+    <view class="head">
+      <div class="user-info">
+        <image
+          class="user-img"
+          src="https://pic2.zhimg.com/50/v2-be54dda1c19aaeab4f90bcb699057d03_hd.jpg"
+        />
+      </div>
+    </view>
+  </view>
+</template>
+<script src="./index.js"></script>
+<style lang="scss" scoped>
+@import "index.scss";
+</style>
