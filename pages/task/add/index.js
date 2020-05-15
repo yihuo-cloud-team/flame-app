ce82e1ec199572af12a2b9a632d2cef124a87d80
代码狮 <1173197065@qq.com>
2020-05-15 17:53:09 +0800

    发布任务样式

--- pages/task/add/index.js
+++ pages/task/add/index.js
@@ -1,13 +1,14 @@
 import uniList from "@/components/uni-list/uni-list.vue"
 import uniListItem from "@/components/uni-list-item/uni-list-item.vue"
 import uniTag from "@/components/uni-tag/uni-tag.vue"
+import Upload from "../../../plugins/Upload"
 
 
 export default {
-    name: 'add',
-    data() {
-        return {
-			form:{
+	name: 'add',
+	data() {
+		return {
+			form: {
 				task_name: "", //任务名称
 				task_type: 0, //任务类型
 				price: "", //任务价格
@@ -25,93 +26,98 @@ export default {
 			priceList: [],
 			show: false
 		};
-    },
-    methods: {
-        // 用于初始化一些数据
-        init() {
-            this.update();
-        },
-        // 用于更新一些数据
-        async update() {
-            const res = await this.$http('/class/list', {});
+	},
+	methods: {
+		// 用于初始化一些数据
+		init() {
+			this.update();
+		},
+		// 用于更新一些数据
+		async update() {
+			const res = await this.$http('/class/list', {});
 			if (res.code >= 0) {
-			    this.classList = res.data;
-			    this.form.task_type = this.classList[0].id;
+				this.classList = res.data;
+				this.form.task_type = this.classList[0].id;
 			}
 			if (!this.isApp) {
-			    this.form.task_type = parseInt(this.$route.query.type)
+				this.form.task_type = parseInt(this.$route.query.type)
 			}
 			// console.warn(res);
-        },
-        go(url) {
-            uni.navigateTo({
-                url: url
-            })
-        },
+		},
+		go(url) {
+			uni.navigateTo({
+				url: url
+			})
+		},
 		change() {
-		    this.classList.forEach(res => {
-		        if (this.form.task_type === res.id) {
-		          this.form.img = this.$getUrl(res.icon);
-		          return
-		        }
-		    })
+			this.classList.forEach(res => {
+				if (this.form.task_type === res.id) {
+					this.form.img = this.$getUrl(res.icon);
+					return
+				}
+			})
 		},
 		select(e) {
-		    this.form.p = e[0].code;
-		    this.form.c = e[1].code;
-		    this.form.a = e[2].code;
-	        this.show = false;
-	    },
+			this.form.p = e[0].code;
+			this.form.c = e[1].code;
+			this.form.a = e[2].code;
+			this.show = false;
+		},
 		price(num) {
-		    this.form.price = num;
+			this.form.price = num;
+		},
+		async upload() {
+			const file = await Upload.select();
+			const res = await Upload.send(file);
+			this.form.img = res.data.url;
 		}
-    },
-    // 计算属性
-    computed: {
+	},
+	// 计算属性
+	computed: {
 		isApp() {
-		      return typeof this.$route.query.type == 'undefined'
+			return typeof this.$route.query.type == 'undefined'
 		}
 	},
-    // 包含 Vue 实例可用过滤器的哈希表。
-    filters: {},
-    // 在实例创建完成后被立即调用
-    created() { },
-    // 在挂载开始之前被调用：相关的 render 函数首次被调用。
-    beforeMount() { },
-    // el 被新创建的 vm.el 替换，并挂载到实例上去之后调用该钩子。
-    mounted() {
-        this.init();
-        this.$nextTick(() => { });
-    },
-    // 数据更新时调用，发生在虚拟 DOM 打补丁之前。
-    beforeUpdate() { },
-    // keep-alive 组件激活时调用。
-    activated() { },
-    // keep-alive 组件停用时调用。
-    deactivated() { },
-    // 实例销毁之前调用。在这一步，实例仍然完全可用。
-    beforeDestroy() { },
-    //Vue 实例销毁后调用。
-    destroyed() { },
-    // 当捕获一个来自子孙组件的错误时被调用。
-    errorCaptured() { },
-    // 包含 Vue 实例可用指令的哈希表。
-    directives: {},
-    // 一个对象，键是需要观察的表达式，值是对应回调函数。
-    watch: {
+	// 包含 Vue 实例可用过滤器的哈希表。
+	filters: {},
+	// 在实例创建完成后被立即调用
+	created() { },
+	// 在挂载开始之前被调用：相关的 render 函数首次被调用。
+	beforeMount() { },
+	// el 被新创建的 vm.el 替换，并挂载到实例上去之后调用该钩子。
+	mounted() {
+		this.init();
+		this.$nextTick(() => { });
+	},
+	// 数据更新时调用，发生在虚拟 DOM 打补丁之前。
+	beforeUpdate() { },
+	// keep-alive 组件激活时调用。
+	activated() { },
+	// keep-alive 组件停用时调用。
+	deactivated() { },
+	// 实例销毁之前调用。在这一步，实例仍然完全可用。
+	beforeDestroy() { },
+	//Vue 实例销毁后调用。
+	destroyed() { },
+	// 当捕获一个来自子孙组件的错误时被调用。
+	errorCaptured() { },
+	// 包含 Vue 实例可用指令的哈希表。
+	directives: {},
+	// 一个对象，键是需要观察的表达式，值是对应回调函数。
+	watch: {
 		'form.task_type': function () {
-		    this.classList.forEach(res => {
-		        if (this.form.task_type === res.id) {
-		          this.priceList = res.price;
-		          console.log(this.priceList)
-		        }
-		    })
+			this.classList.forEach(res => {
+				if (this.form.task_type === res.id) {
+					this.priceList = res.price;
+					console.log(this.priceList)
+				}
+			})
 		}
 	},
-    // 组件列表
-    components: {
-        uniList,
-        uniListItem,
+	// 组件列表
+	components: {
+		uniList,
+		uniListItem,
 		uniTag
-    },
+	},
 };
--- pages/task/add/index.vue
+++ pages/task/add/index.vue
@@ -1,90 +1,96 @@
 <template>
-	<view id="add">
-	    <view class="name">
-	      <view class="title">任务名称</view>
-	      <view class="input">
-			  <input type="text" maxlength="30" v-model="form.task_name" placeholder="30字符以内"/>
-	        
-	      </view>
-	    </view>
-	
-	    <view class="tupian">
-	      <view class="title">任务图片</view>
-	      <view class="store-image">
-	        
-			<!-- color: rgba(66, 137, 220, 1)-->
-	        <view class="cu-tag line-blue" @click="change">默认图片</view>
-	      </view>
-	    </view>
-	    <view class="task-type">
-	      <view class="title">任务类别</view>
-	      <view class="option">
-	        <view v-for="(item,i) in classList" :key='i' :class="['item',{'activ': form.task_type == item.id}]" @click="form.task_type= item.id">{{item.name}}</view>
-	      </view>
-	    </view>
-	    <view class="price">
-	      <view class="title">任务价格</view>
-	      <view class="price-box" v-if="priceList">
-			<view class="cu-tag bg-red" style="margin:5px;" @click="price(todo)" v-for="(todo,i) in priceList" :key='i'>{{todo}}</view>
-	        <!-- <uni-tag class="tag" @click="price(todo)" v-for="(todo,i) in priceList" :key='i' :text="todo+''" type="danger" size="small" inverted></uni-tag> -->
-	      </view>
-	      <view class="input">
-	        <input maxlength="30" type="number" v-model.number="form.price" placeholder="请输入数字" />
-	      </view>
-	    </view>
-	    <view class="address">
-	      <view class="title">任务周期(天)</view>
-	      <view class="input">
-	        <input maxlength="30" type="number" v-model.number="form.cycle" placeholder="请输入数字" />
-	      </view>
-	    </view>
-	    <view class="info">
-	      <view class="title">任务信息</view>
-	      <view class="input">
-	        <textarea
-	          style="padding: 10px;"
-	          v-model="form.info"
-	          maxlength="300"
-	          placeholder="请尽量详细描述您的项目，以吸引更多人报名"
-	          show-word-limit
-	        />
-	      </view>
-	    </view>
-	    <view class="region">
-	      <view class="title">所在地</view>
-	      <view class="input" v-if="form.p==''" @click="show = !show">省市区选择</view>
-	      <view
-	        class="input"
-	        v-if="form.p!=''"
-	        @click="show = !show"
-	      >{{form.p | areaToStr}}{{form.c | areaToStr}}{{form.a | areaToStr}}</view>
-	    </view>
-	    <view class="address">
-	      <view class="title">详细地址</view>
-	      <view class="input">
-	        <input maxlength="10" v-model="form.address" placeholder="10字符以内" />
-	      </view>
-	    </view>
-	    <view class="address">
-	      <view class="title">
-	        联系方式
-	        <span style="font-size:14px">(手机/微信/QQ等)</span>
-	      </view>
-	      <view class="input">
-	        <input v-model="form.contact" placeholder="手机、微信、QQ等" />
-	      </view>
-	    </view>
-	
-	    <view class="btn" @click="submit">下一步：托管资金</view>
-	    <!-- <van-popup v-model="show" position="bottom">
+  <view id="add">
+    <view class="name">
+      <view class="title">任务名称</view>
+      <view class="input">
+        <input type="text" maxlength="30" v-model="form.task_name" placeholder="30字符以内" />
+      </view>
+    </view>
+
+    <view class="tupian">
+      <view class="title">任务图片</view>
+      <image mode="aspectFill" class="task-img" @click="upload" :src="$getUrl(form.img)" />
+      <view class="default-text">
+        <text @click="change">使用默认图片</text>
+      </view>
+    </view>
+    <view class="task-type">
+      <view class="title">任务类别</view>
+      <view class="option">
+        <view
+          v-for="(item,i) in classList"
+          :key="i"
+          :class="['item',{'activ': form.task_type == item.id}]"
+          @click="form.task_type= item.id"
+        >
+          <view>{{item.name}}</view>
+        </view>
+      </view>
+    </view>
+    <view class="price">
+      <view class="title">任务价格</view>
+      <view class="price-box" v-if="priceList">
+        <view class="price-card" @click="price(todo)" v-for="(todo,i) in priceList" :key="i">
+          <view>{{todo}}</view>
+        </view>
+        <!-- <uni-tag class="tag" @click="price(todo)" v-for="(todo,i) in priceList" :key='i' :text="todo+''" type="danger" size="small" inverted></uni-tag> -->
+      </view>
+      <view class="input">
+        <input maxlength="30" type="number" v-model.number="form.price" placeholder="请输入数字" />
+      </view>
+    </view>
+    <view class="address">
+      <view class="title">任务周期(天)</view>
+      <view class="input">
+        <input maxlength="30" type="number" v-model.number="form.cycle" placeholder="请输入数字" />
+      </view>
+    </view>
+    <view class="info">
+      <view class="title">任务信息</view>
+      <view class="input">
+        <textarea
+          style="padding: 10px;"
+          v-model="form.info"
+          maxlength="300"
+          placeholder="请尽量详细描述您的项目，以吸引更多人报名"
+          show-word-limit
+        />
+      </view>
+    </view>
+    <view class="region">
+      <view class="title">所在地</view>
+      <view class="input" v-if="form.p==''" @click="show = !show">省市区选择</view>
+      <view
+        class="input"
+        v-if="form.p!=''"
+        @click="show = !show"
+      >{{form.p | areaToStr}}{{form.c | areaToStr}}{{form.a | areaToStr}}</view>
+    </view>
+    <view class="address">
+      <view class="title">详细地址</view>
+      <view class="input">
+        <input maxlength="10" v-model="form.address" placeholder="10字符以内" />
+      </view>
+    </view>
+    <view class="address">
+      <view class="title">
+        联系方式
+        <span style="font-size:14px">(手机/微信/QQ等)</span>
+      </view>
+      <view class="input">
+        <input v-model="form.contact" placeholder="手机、微信、QQ等" />
+      </view>
+    </view>
+
+    <view class="btn" @click="submit">下一步：托管资金</view>
+    <!-- <van-popup v-model="show" position="bottom">
 	      <van-area :area-list="areaList" value="110101" @cancel="show = !show" @confirm="select" />
-	    </van-popup> -->
-	  </view>
-	</view>
+    </van-popup>-->
+  </view>
 </template>
 
 <script src="./index.js"></script>
 
 <style lang="scss" scoped>
-	@import "index.scss";
+@import "index.scss";
 </style>
