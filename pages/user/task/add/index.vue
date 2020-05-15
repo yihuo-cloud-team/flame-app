0e0ba5a13e453acde21cc2b5432096baddd4d271
wang9973 <33651319+wang9973@users.noreply.github.com>
2020-05-15 15:47:56 +0800

    re

---
+++ pages/user/task/add/index.vue
@@ -0,0 +1,92 @@
+<template>
+	<view id="add">
+	    <view class="name">
+	      <view class="title">任务名称</view>
+	      <view class="input">
+			  <input type="text" maxlength="30" v-model="form.task_name" placeholder="30字符以内"/>
+	        
+	      </view>
+	    </view>
+	
+	    <view class="tupian">
+	      <view class="title">任务图片</view>
+	      <view class="store-image">
+	        <ol-upload style="border-radius:100%;">
+	          <image :class="form.img==''?'img':'img1'" :src="$getUrl(form.img)" mode='widthFix'/>
+	        </ol-upload>
+			<!-- color: rgba(66, 137, 220, 1)-->
+	        <uni-tag class="tag" text="默认图片" @click="change" plain inverted size="small" type="primary"></uni-tag>
+	      </view>
+	    </view>
+	    <view class="task-type">
+	      <view class="title">任务类别</view>
+	      <view class="option">
+	        <view v-for="(item,i) in classList" :key='i' :class="['item',{'activ': form.task_type == item.id}]" @click="form.task_type= item.id">{{item.name}}</view>
+	      </view>
+	    </view>
+	    <view class="price">
+	      <view class="title">任务价格</view>
+	      <view class="price-box" v-if="priceList">
+			  <!-- <span @click="price(todo)" v-for="(todo,i) in priceList" :key='i'>{{todo}}</span> -->
+	        <uni-tag class="tag" @click="price(todo)" v-for="(todo,i) in priceList" :key='i' :text="todo+''" type="danger" size="small" inverted></uni-tag>
+	      </view>
+	      <view class="input">
+	        <input maxlength="30" type="number" v-model.number="form.price" placeholder="请输入数字" />
+	      </view>
+	    </view>
+	    <view class="address">
+	      <view class="title">任务周期(天)</view>
+	      <view class="input">
+	        <input maxlength="30" type="number" v-model.number="form.cycle" placeholder="请输入数字" />
+	      </view>
+	    </view>
+	    <view class="info">
+	      <view class="title">任务信息</view>
+	      <view class="input">
+	        <textarea
+	          style="padding: 10px;"
+	          v-model="form.info"
+	          maxlength="300"
+	          placeholder="请尽量详细描述您的项目，以吸引更多人报名"
+	          show-word-limit
+	        />
+	      </view>
+	    </view>
+	    <view class="region">
+	      <view class="title">所在地</view>
+	      <view class="input" v-if="form.p==''" @click="show = !show">省市区选择</view>
+	      <view
+	        class="input"
+	        v-if="form.p!=''"
+	        @click="show = !show"
+	      >{{form.p | areaToStr}}{{form.c | areaToStr}}{{form.a | areaToStr}}</view>
+	    </view>
+	    <view class="address">
+	      <view class="title">详细地址</view>
+	      <view class="input">
+	        <input maxlength="10" v-model="form.address" placeholder="10字符以内" />
+	      </view>
+	    </view>
+	    <view class="address">
+	      <view class="title">
+	        联系方式
+	        <span style="font-size:14px">(手机/微信/QQ等)</span>
+	      </view>
+	      <view class="input">
+	        <input v-model="form.contact" placeholder="手机、微信、QQ等" />
+	      </view>
+	    </view>
+	
+	    <view class="btn" @click="submit">下一步：托管资金</view>
+	    <!-- <van-popup v-model="show" position="bottom">
+	      <van-area :area-list="areaList" value="110101" @cancel="show = !show" @confirm="select" />
+	    </van-popup> -->
+	  </view>
+	</view>
+</template>
+
+<script src="./index.js"></script>
+
+<style lang="scss" scoped>
+	@import "index.scss";
+</style>
