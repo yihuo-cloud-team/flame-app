35e47205735c1447f98a652e18dfc67ada1851e1
YorkZhangZ <18221196274@163.com>
2020-05-26 21:03:31 +0800

    推送（未完）

--- pages/login/index.js
+++ pages/login/index.js
@@ -6,6 +6,7 @@ export default {
 			msg: '',
 			checked: false,
 			isShow: false,
+			cid: '',
 		};
 	},
 	methods: {
@@ -17,25 +18,23 @@ export default {
 		async update() {
 			// const res = await this.$http('', {});
 		},
-		async submit() { },
+		async submit() {},
 		async wxLogin() {
 			var _this = this;
 			uni.login({
 				provider: 'weixin',
-				success: function (loginRes) {
+				success: function(loginRes) {
 					console.log(loginRes.authResult);
 					uni.getUserInfo({
 						provider: 'weixin',
-						success: async function (infoRes) {
+						success: async function(infoRes) {
 							console.log(infoRes)
 							_this.userInfo = _this.$twoJsonMerge(loginRes.authResult, infoRes)
-							// let openid = loginRes.authResult.openid;
-							// let access_token = loginRes.authResult.access_token;
-							// let unionid = loginRes.authResult.unionid;
-							let userInfo=infoRes.userInfo;
+							let userInfo = infoRes.userInfo;
+							userInfo.cid = this.cid;
 							console.log(userInfo)
-							const res = await _this.$http('/auth/app_wx_login',  userInfo );
-							
+							const res = await _this.$http('/auth/app_wx_login', userInfo);
+
 							console.log(res)
 							if (res.code >= 1) {
 								uni.setStorageSync('jwt', res.jwt);
@@ -47,13 +46,13 @@ export default {
 								_this.errPopup(_this.msg);
 							}
 						},
-						fail: function (error) {
+						fail: function(error) {
 							console.log(error.errMsg);
 							_this.errPopup(error.errMsg);
 						}
 					});
 				},
-				fail: function (error) {
+				fail: function(error) {
 					console.log(error.errMsg);
 					_this.errPopup(error.errMsg);
 				}
@@ -71,33 +70,36 @@ export default {
 	},
 	// 计算属性
 	computed: {
-		jsonData: function (json) {
+		jsonData: function(json) {
 			return JSON.stringify(this.userInfo)
 		}
 	},
 	// 包含 Vue 实例可用过滤器的哈希表。
 	filters: {},
 	// 在实例创建完成后被立即调用
-	created() { },
+	created() {},
 	// 在挂载开始之前被调用：相关的 render 函数首次被调用。
-	beforeMount() { },
+	beforeMount() {},
 	// el 被新创建的 vm.el 替换，并挂载到实例上去之后调用该钩子。
 	mounted() {
 		this.init();
-		this.$nextTick(() => { });
+		// 页面加载时触发  
+		this.cid = plus.push.getClientInfo().clientid;//客户端标识  
+		console.log(this.cid)
+		this.$nextTick(() => {});
 	},
 	// 数据更新时调用，发生在虚拟 DOM 打补丁之前。
-	beforeUpdate() { },
+	beforeUpdate() {},
 	// keep-alive 组件激活时调用。
-	activated() { },
+	activated() {},
 	// keep-alive 组件停用时调用。
-	deactivated() { },
+	deactivated() {},
 	// 实例销毁之前调用。在这一步，实例仍然完全可用。
-	beforeDestroy() { },
+	beforeDestroy() {},
 	//Vue 实例销毁后调用。
-	destroyed() { },
+	destroyed() {},
 	// 当捕获一个来自子孙组件的错误时被调用。
-	errorCaptured() { },
+	errorCaptured() {},
 	// 包含 Vue 实例可用指令的哈希表。
 	directives: {},
 	// 一个对象，键是需要观察的表达式，值是对应回调函数。
